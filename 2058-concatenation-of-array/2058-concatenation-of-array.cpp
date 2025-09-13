2f689967040e11125eb8734d80b3aab904fe7639
Muhammad Younas Peerzada <pakistan14612@gmail.com>
2025-09-13 13:00:31 +0500

    Time: 0 ms (100%), Space: 16.9 MB (43.28%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> ans(2 * n, 1);
+        vector<int> ans(2 * n);
 
         for (int i = 0; i < n; i++) {
             ans[i] = nums[i];
